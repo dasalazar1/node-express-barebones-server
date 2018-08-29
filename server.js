49ff93f9debd7393bd448231136af9c6d2a6bbc8
daniel salazar <dasalazar1@gmail.com>
2018-08-29 10:48:27 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var routes = require('./src/router/index.js');
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/controllers', express.static(process.cwd() + '/src/controllers'));
+
+routes(app);
+
+app.listen(3500, function () {
+  console.log('Node.js listening on port 3000...');
+});
